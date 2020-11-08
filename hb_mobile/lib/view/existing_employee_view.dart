990b0ac855a512b1ea02e24ecab6aa65fab62048
naveenkumar <www.naveenkumar24637@gmail.com>
2020-11-08 14:08:03 +0530

    fix existing employee ui

--- hb_mobile/lib/view/existing_employee_view.dart
+++ hb_mobile/lib/view/existing_employee_view.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hb_mobile/constant.dart';
 import 'package:hb_mobile/widgets/common_widgets.dart';
+import 'package:hb_mobile/widgets/delete_card.dart';
+import 'package:hb_mobile/widgets/profile_card.dart';
+import 'package:hb_mobile/widgets/search_widget.dart';
 
 class ExistingEmployeesScreen extends StatelessWidget {
   @override
@@ -106,17 +109,24 @@ class _ExistingEmployeesListState extends State<ExistingEmployeesList> {
   Widget _buildCards(EmployeeLoadedState state, List<Employee> employees) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
+        SizedBox(height: 8.0),
+        SearchWidget(
           child: TextField(
+            style: TextStyle(color: Colors.white),
             onChanged: (query) => _viewEmployeeBloc
                 .add(SearchAndFetchEmployeeEvent(ename: query)),
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Search employees'),
+            decoration: kSearchTextFieldDecoration,
           ),
         ),
+        SizedBox(height: 8.0),
         Expanded(
-          child: _buildCardList(state, employees),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              await Future.delayed(Duration(seconds: 1));
+              _viewEmployeeBloc.add(FetchEmployeeEvent());
+            },
+            child: _buildCardList(state, employees),
+          ),
         ),
       ],
     );
@@ -135,59 +145,96 @@ class _ExistingEmployeesListState extends State<ExistingEmployeesList> {
   }
 
   Widget _buildCard(List<Employee> employees, int index, BuildContext context) {
-    return ExpansionTile(
-      title: Text('${employees[index].ename}'),
-      subtitle: Text('${employees[index].enumber}'),
-      trailing: Text('${employees[index].eaddate}'),
-      children: [
-        Row(
-          children: [
-            Padding(
-              padding: kHorizontalPadding,
-              child: Text('${employees[index].eaddress}'),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Padding(
-              padding: kHorizontalPadding,
-              child: Text('${employees[index].ecode}'),
-            ),
-            IconButton(
-              onPressed: () {
-                _showModalBottomSheet(context, employees, index);
-              },
-              icon: Icon(Icons.edit),
-            ),
-            IconButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    title: Text(
-                      'Are you sure you want to delete "${employees[index].ename}"?',
-                    ),
-                    actions: [
-                      FlatButton(
-                          onPressed: () {
-                            _editEmployeeBloc.add(
-                                DeleteEmployee(ecode: employees[index].ecode));
-                            Navigator.pop(context);
-                          },
-                          child: Text('Yes')),
-                      FlatButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: Text('No')),
-                    ],
-                  ),
-                );
-              },
-              icon: Icon(Icons.delete),
-            ),
-          ],
-        ),
+    return ProfileCard(
+      cardKey: "${employees[index].ecode}",
+      title: "${employees[index].ename}",
+      subtitle: "${employees[index].enumber}",
+      date: "${employees[index].eaddate}",
+      id: "${employees[index].ecode}",
+      detail: "${employees[index].eaddress}",
+      editCard: _editCard(context, employees, index),
+      deleteCard: _deleteCard(context, employees, index),
+    );
+    // return ExpansionTile(
+    //   title: Text('${employees[index].ename}'),
+    //   subtitle: Text('${employees[index].enumber}'),
+    //   trailing: Text('${employees[index].eaddate}'),
+    //   children: [
+    //     Row(
+    //       children: [
+    //         Padding(
+    //           padding: kHorizontalPadding,
+    //           child: Text('${employees[index].eaddress}'),
+    //         ),
+    //       ],
+    //     ),
+    //     Row(
+    //       children: [
+    //         Padding(
+    //           padding: kHorizontalPadding,
+    //           child: Text('${employees[index].ecode}'),
+    //         ),
+    //         IconButton(
+    //           onPressed: () {
+    //             _showModalBottomSheet(context, employees, index);
+    //           },
+    //           icon: Icon(Icons.edit),
+    //         ),
+    //         IconButton(
+    //           onPressed: () {
+    //             showDialog(
+    //               context: context,
+    //               builder: (_) => AlertDialog(
+    //                 title: Text(
+    //                   'Are you sure you want to delete "${employees[index].ename}"?',
+    //                 ),
+    //                 actions: [
+    //                   FlatButton(
+    //                       onPressed: () {
+    //                         _editEmployeeBloc.add(
+    //                             DeleteEmployee(ecode: employees[index].ecode));
+    //                         Navigator.pop(context);
+    //                       },
+    //                       child: Text('Yes')),
+    //                   FlatButton(
+    //                       onPressed: () => Navigator.pop(context),
+    //                       child: Text('No')),
+    //                 ],
+    //               ),
+    //             );
+    //           },
+    //           icon: Icon(Icons.delete),
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+    // );
+  }
+
+  Widget _deleteCard(
+      BuildContext context, List<Employee> employees, int index) {
+    return DeleteCard(
+      title: 'Are you sure you want to delete "${employees[index].ename}"?',
+      actions: [
+        FlatButton(
+            onPressed: () {
+              _editEmployeeBloc
+                  .add(DeleteEmployee(ecode: employees[index].ecode));
+              Navigator.pop(context);
+            },
+            child: Text('Yes')),
+        FlatButton(onPressed: () => Navigator.pop(context), child: Text('No')),
       ],
+    );
+  }
+
+  IconButton _editCard(
+      BuildContext context, List<Employee> employees, int index) {
+    return IconButton(
+      onPressed: () {
+        _showModalBottomSheet(context, employees, index);
+      },
+      icon: Icon(Icons.edit),
     );
   }
 

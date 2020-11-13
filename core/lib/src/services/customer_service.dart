4fd76308829ca4cf18f91ea749971813f0dfe9d5
naveenkumar <www.naveenkumar24637@gmail.com>
2020-11-13 16:46:43 +0530

    add customer service

--- core/lib/src/services/customer_service.dart
+++ core/lib/src/services/customer_service.dart
@@ -1,14 +1,14 @@
-// import 'package:core/src/business_logics/models/response_result.dart';
-// import 'package:core/src/business_logics/models/employee.dart';
-//
-// abstract class EmployeeService {
-//   Future<ResponseResult> submitEmployee(Employee employee);
-//
-//   Future<ResponseResult> editEmployeeByCode(Employee employee);
-//
-//   Future<ResponseResult> deleteEmployee(Map<String, dynamic> ecode);
-//
-//   Future<Employees> getAllEmployees();
-//
-//   Future<Employee> getEmployeeByName(String employeeName);
-// }
+import 'package:core/src/business_logics/models/response_result.dart';
+import 'package:core/src/business_logics/models/customer.dart';
+
+abstract class CustomerService {
+  Future<ResponseResult> submitCustomer(Customer customer);
+
+  Future<ResponseResult> editCustomerByCode(Customer customer);
+
+  Future<ResponseResult> deleteCustomer(Map<String, dynamic> ecode);
+
+  Future<Customers> getAllCustomers();
+
+  Future<Customer> getCustomerByName(String customerName);
+}

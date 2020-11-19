8cc58dbb0c4e3c08393d0b2ebbb21a86b8b0fc39
naveenkumar <www.naveenkumar24637@gmail.com>
2020-11-19 18:23:34 +0530

    add generate date by custom input

--- core/lib/src/business_logics/util/generate_date.dart
+++ core/lib/src/business_logics/util/generate_date.dart
@@ -1,10 +1,23 @@
-String generateDate() {
-  String day = DateTime.now().day < 10
-      ? '0${DateTime.now().day}'
-      : '${DateTime.now().day}';
-  String month = DateTime.now().month < 10
-      ? '0${DateTime.now().month}'
-      : '${DateTime.now().month}';
-  int year = DateTime.now().year;
-  return '$day-$month-$year';
+String generateDate({DateTime selectedDate}) {
+  String day, month;
+  int year;
+
+  if (selectedDate != null) {
+    day =
+        selectedDate.day < 10 ? '0${selectedDate.day}' : '${selectedDate.day}';
+    month = selectedDate.month < 10
+        ? '0${selectedDate.month}'
+        : '${selectedDate.month}';
+    year = selectedDate.year;
+    return '$day-$month-$year';
+  } else {
+    day = DateTime.now().day < 10
+        ? '0${DateTime.now().day}'
+        : '${DateTime.now().day}';
+    month = DateTime.now().month < 10
+        ? '0${DateTime.now().month}'
+        : '${DateTime.now().month}';
+    year = DateTime.now().year;
+    return '$day-$month-$year';
+  }
 }

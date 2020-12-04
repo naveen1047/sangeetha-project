c1202b375b3e534813825eccc060174c38fc98f9
naveenkumar <www.naveenkumar24637@gmail.com>
2020-12-04 18:23:45 +0530

    refactor MP entry view

--- core/lib/src/business_logics/bloc/date_picker_bloc/date_picker_bloc.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:core/src/business_logics/util/generate_date.dart';
-
-class DatePickerCubit extends Cubit<String> {
-  DatePickerCubit(String state) : super(generateDate());
-
-  void selectDate(DateTime dateTime) => generateDate(selectedDate: dateTime);
-}
